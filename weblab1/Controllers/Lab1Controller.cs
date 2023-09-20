4a5cbe8c010ffb7d45a7fbc2043d3718cc3ec6b1
Valeria-i <ValSam.111iq@gmail.com>
2023-09-20 11:28:19 +0700

    <REqued>

---
+++ weblab1/Controllers/Lab1Controller.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace weblab1.Controllers
+{
+    public class Lab1Controller : Controller
+    {
+        // GET: Lab1
+        public ActionResult FirstViewMethod()
+        {
+            List<string> fructs = GetFructsList();
+            return View(fructs);
+        }
+        public ActionResult SecondViewMethod()
+        {
+            return View(GetFructsList().OrderBy(x => x).ToList());
+        }
+        public ActionResult ThirdViewMethod()
+        {
+            return View(GetFructsList().OrderBy(x => x).ToList());
+        
+        }
+        public List<string> GetFructsList()
+        {
+            List<string> fructs = new List<string>();
+            fructs.Add("Яблоко");
+            fructs.Add("Ананас");
+            fructs.Add("Киви");
+            fructs.Add("Мандарин");
+            fructs.Add("Манго");
+            fructs.Add("Груша");
+            fructs.Add("Персик");
+            return fructs;
+        }
+
+        public ActionResult MyViewMethod()
+        {
+            return View(GetFructsList().OrderBy(x => x).ToList());
+
+        }
+
+        public List<string> GetCoffeeList()
+        {
+            List<string> coffee = new List<string>();
+            coffee.Add("Эспрессо");
+            coffee.Add("Американо");
+            coffee.Add("Латте");
+            coffee.Add("Капучино");
+            coffee.Add("Раф");
+            return coffee;
+        }
+        public ActionResult tusckviewMethod()
+        {
+           
+            return View(GetCoffeeList().OrderByDescending(x=>x.Length).ToList());
+        }
+    }
+}
---
+++ weblab1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="weblab1.MvcApplication" Language="C#" %>
